f77ac04a1db3011891e8339e2cb9e6afb34c98ff
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-17 16:35:39 +0000

    create helm release rebase

--- helm_cert_manager.tf
+++ helm_cert_manager.tf
@@ -4,11 +4,11 @@ resource "helm_release" "cert-manager" {
   name = "cert-manager"
   # repository = "https://charts.jetstack.io"
   chart = "./helm/cert-manager"
-  namespace = var.cert_manager_namespace
+  namespace = var.kubernetes_namespace.cert-manager
 
   values = [
     templatefile(
-      "${path.module}/helm/cert-manager/values.yaml.tpl",
+      "${path.module}/template/cert-manager/values.yaml.tpl",
       {
         "cert_chart_version" = var.cert_chart_version
         "cert_app_version" = var.cert_app_version
