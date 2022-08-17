40c5143634b1c78c6a25a179ba36633c11bfc8f7
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-17 16:35:39 +0000

    create helm release

---
+++ helm_cert_manager.tf
@@ -0,0 +1,23 @@
+resource "helm_release" "cert-manager" {
+  depends_on = [kubernetes_namespace.cert-manager]
+  
+  name = "cert-manager"
+  # repository = "https://charts.jetstack.io"
+  chart = "./helm/cert-manager"
+  namespace = var.kubernetes_namespace.cert-manager
+
+  values = [
+    templatefile(
+      "${path.module}/template/cert-manager/values.yaml.tpl",
+      {
+        "cert_chart_version" = var.cert_chart_version
+        "cert_app_version" = var.cert_app_version
+      }
+    )
+
+    set {
+      name = "webhook.extraArgs"
+      value = "{--featureGates=\"AdditionalCertificateOutputFormats\"}"
+    }
+  ]
+}
