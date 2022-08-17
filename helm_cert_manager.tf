4382d25eb1f8d383d0224c099d74cf5c6d71b17e
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-17 17:00:14 +0000

    edit syntax

--- helm_cert_manager.tf
+++ helm_cert_manager.tf
@@ -4,7 +4,7 @@ resource "helm_release" "cert-manager" {
   name = "cert-manager"
   # repository = "https://charts.jetstack.io"
   chart = "./helm/cert-manager"
-  namespace = var.kubernetes_namespace.cert-manager
+  namespace = var.cert_manager_namespace
 
   values = [
     templatefile(
