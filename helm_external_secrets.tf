4937569bb302fbd20a373cdc1690fbac02c20647
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-18 11:14:25 +0300

    use set for variables for EOF (#15)

--- helm_external_secrets.tf
+++ helm_external_secrets.tf
@@ -1,16 +1,20 @@
 resource "helm_release" "external-secrets" {
   depends_on = [kubernetes_namespace.external-secrets]
 
-  name = "external-secrets"
-  namespace = var.external_secrets_namespace
-  chart = "./charts/external-secrets"
+  name              = "external-secrets"
+  namespace         = var.external_secrets_namespace
+  chart             = "./charts/external-secrets"
   dependency_update = true
 
   values = [
     <<-EOF
     external-secrets:
       installCRDs: true
-      es_version: ${var.external_secrets_version}
     EOF
   ]
+
+  set {
+    name  = "es_version"
+    value = var.external_secrets_version
+  }
 }
