50a99ac7e48b6efc5f40aa81f0021137e1d1df4d
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-18 20:14:51 +0000

    edit cert manager namespace variable

--- helm_cert_manager.tf
+++ helm_cert_manager.tf
@@ -10,7 +10,7 @@ resource "helm_release" "cert-manager" {
   values = [templatefile("${path.module}/charts/cert-manager/values.yaml.tpl",
     {
       "gcp_secret_sa" = var.gcp_secret_sa
-      "cert_namespace" = var.cert_namespace
+      "cert_namespace" = var.cert_manager_namespace
       "gcp_secret_sa_credentials" = var.gcp_secret_sa_credentials
       "dns_service_account" = var.service_account
     }
