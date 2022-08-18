1fdbb16d3de38ec1701c67afc075f0477caf0752
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-18 10:05:39 +0000

    resolve conflicts

--- helm_external_secrets.tf
+++ helm_external_secrets.tf
@@ -6,10 +6,11 @@ resource "helm_release" "external-secrets" {
   chart             = "./charts/external-secrets"
   dependency_update = true
 
-  values = [templatefile("${path.module}/charts/external-secrets.values.yaml.tpl"),
-  {
-    "es_version" = var.external_secrets_version
-  }
+  values = [templatefile("${path.module}/charts/external-secrets.values.yaml.tpl",
+      {
+        "es_version" = var.external_secrets_version
+      }
+    )
   ]
 
   set {
