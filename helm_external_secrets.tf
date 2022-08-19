1fb5bcac0aec37b5ce37372a37fa0f4cfbe38b05
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-19 09:10:55 +0000

    remove versions

--- helm_external_secrets.tf
+++ helm_external_secrets.tf
@@ -5,7 +5,6 @@ resource "helm_release" "external-secrets" {
   namespace         = var.external_secrets_namespace
   repository        = "https://charts.external-secrets.io"
   chart             = "external-secrets/external-secrets"
-  version           = "v0.5.9"
 
   set {
     name  = "installCRDs"
@@ -21,6 +20,5 @@ resource "helm_release" "reflector" {
   namespace         = var.external_secrets_namespace
   repository        = "https://emberstack.github.io/helm-charts"
   chart             = "emberstack/reflector"
-  version           = "6.1.47"
 
 }
