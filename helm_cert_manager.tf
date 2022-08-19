d8259a6d58bf1dd0370c42a15b416f77342c0631
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-19 05:13:42 +0000

    update external-secrets chart name

--- helm_cert_manager.tf
+++ helm_cert_manager.tf
@@ -2,7 +2,6 @@ resource "helm_release" "cert-manager" {
   depends_on = [kubernetes_namespace.cert-manager, google_container_node_pool.node_pool]
 
   name = "cert-manager"
-  # repository = "https://charts.jetstack.io"
   chart             = "./charts/cert-manager"
   namespace         = var.cert_manager_namespace
   dependency_update = true
