cb37cab9a50741968a51984c62a507960dd96102
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-19 12:15:56 +0000

    install cert-manager in isolation

--- helm_cert_manager.tf
+++ helm_cert_manager.tf
@@ -1,20 +1,31 @@
 resource "helm_release" "cert-manager" {
   depends_on = [kubernetes_namespace.cert-manager, google_container_node_pool.node_pool]
 
-  name              = "cert-manager"
-  chart             = "./charts/cert-manager"
-  namespace         = var.cert_manager_namespace
-  dependency_update = true
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  namespace  = var.cert_manager_namespace
+  version    = "v1.9.1"
 
-  values = [templatefile("${path.module}/charts/cert-manager/values.yaml.tpl",
-    {
-      "gcp_secret_sa"             = var.gcp_secret_sa
-      "cert_namespace"            = var.cert_manager_namespace
-      "es_namespace"              = var.external_secrets_namespace
-      "gcp_secret_sa_credentials" = var.gcp_secret_sa_credentials
-      "dns_service_account"       = var.service_account_base64
-    }
-  )]
+  set {
+    name  = "installCRDs"
+    value = true
+  }
+
+  set {
+    name  = "enable-certificate-owner-ref"
+    value = true
+  }
+
+  set {
+    name  = "featureGates"
+    value = "AdditionalCertificateOutputFormats=true"
+  }
+
+  set {
+    name  = "prometheus.enabled"
+    value = false
+  }
 
   set {
     name  = "webhook.extraArgs"
