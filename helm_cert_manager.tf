80a1705f24db39c7bee54a0bc249eb739febbaf7
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-20 16:48:21 +0000

    uninstall cert manager

--- helm_cert_manager.tf
+++ helm_cert_manager.tf
@@ -1,39 +1,39 @@
-resource "helm_release" "cert-manager" {
-  depends_on = [kubernetes_namespace.cert-manager, google_container_node_pool.node_pool]
-
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  namespace  = var.cert_manager_namespace
-  version    = "v1.9.1"
-
-  set {
-    name  = "installCRDs"
-    value = true
-  }
-
-  set {
-    name  = "enable-certificate-owner-ref"
-    value = true
-  }
-
-  set {
-    name  = "featureGates"
-    value = "AdditionalCertificateOutputFormats=true,"
-  }
-
-  set {
-    name = "extraArgs"
-    value = format("{%s,%s,%s}","--dns01-recursive-nameservers-only", "--dns01-recursive-nameservers=8.8.8.8:53", "1.1.1.1:53")
-  }
-
-  set {
-    name  = "prometheus.enabled"
-    value = false
-  }
-
-  set {
-    name  = "webhook.extraArgs"
-    value = "{--feature-gates=AdditionalCertificateOutputFormats=true}"
-  }
-}
+#resource "helm_release" "cert-manager" {
+#  depends_on = [kubernetes_namespace.cert-manager, google_container_node_pool.node_pool]
+#
+#  name       = "cert-manager"
+#  repository = "https://charts.jetstack.io"
+#  chart      = "cert-manager"
+#  namespace  = var.cert_manager_namespace
+#  version    = "v1.9.1"
+#
+#  set {
+#    name  = "installCRDs"
+#    value = true
+#  }
+#
+#  set {
+#    name  = "enable-certificate-owner-ref"
+#    value = true
+#  }
+#
+#  set {
+#    name  = "featureGates"
+#    value = "AdditionalCertificateOutputFormats=true,"
+#  }
+#
+#  set {
+#    name = "extraArgs"
+#    value = format("{%s,%s,%s}","--dns01-recursive-nameservers-only", "--dns01-recursive-nameservers=8.8.8.8:53", "1.1.1.1:53")
+#  }
+#
+#  set {
+#    name  = "prometheus.enabled"
+#    value = false
+#  }
+#
+#  set {
+#    name  = "webhook.extraArgs"
+#    value = "{--feature-gates=AdditionalCertificateOutputFormats=true}"
+#  }
+#}
