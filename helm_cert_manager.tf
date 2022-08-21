84a7aaed467da015a17c9efb87516ff330f3daca
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-08-21 07:33:11 +0000

    uninstall

--- helm_cert_manager.tf
+++ helm_cert_manager.tf
@@ -1,38 +1,38 @@
-resource "helm_release" "cert-manager" {
-  depends_on = [kubernetes_namespace.cert-manager, google_container_node_pool.node_pool]
-
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  namespace  = var.cert_manager_namespace
-  version    = "v1.9.1"
-
-  set {
-    name  = "installCRDs"
-    value = true
-  }
-
-  set {
-    name  = "featureGates"
-    value = "AdditionalCertificateOutputFormats=true,"
-  }
-
-  set {
-    name  = "extraArgs"
-    value = format("{%s,%s,%s\\,%s}",
-      "--enable-certificate-owner-ref=true",
-      "--dns01-recursive-nameservers-only",
-      "--dns01-recursive-nameservers=8.8.8.8:53",
-      "1.1.1.1:53")
-  }
-
-  set {
-    name  = "prometheus.enabled"
-    value = true
-  }
-
-  set {
-    name  = "webhook.extraArgs"
-    value = "{--feature-gates=AdditionalCertificateOutputFormats=true}"
-  }
-}
+#resource "helm_release" "cert-manager" {
+#  depends_on = [kubernetes_namespace.cert-manager, google_container_node_pool.node_pool]
+#
+#  name       = "cert-manager"
+#  repository = "https://charts.jetstack.io"
+#  chart      = "cert-manager"
+#  namespace  = var.cert_manager_namespace
+#  version    = "v1.9.1"
+#
+#  set {
+#    name  = "installCRDs"
+#    value = true
+#  }
+#
+#  set {
+#    name  = "featureGates"
+#    value = "AdditionalCertificateOutputFormats=true,"
+#  }
+#
+#  set {
+#    name  = "extraArgs"
+#    value = format("{%s,%s,%s\\,%s}",
+#      "--enable-certificate-owner-ref=true",
+#      "--dns01-recursive-nameservers-only",
+#      "--dns01-recursive-nameservers=8.8.8.8:53",
+#      "1.1.1.1:53")
+#  }
+#
+#  set {
+#    name  = "prometheus.enabled"
+#    value = true
+#  }
+#
+#  set {
+#    name  = "webhook.extraArgs"
+#    value = "{--feature-gates=AdditionalCertificateOutputFormats=true}"
+#  }
+#}
