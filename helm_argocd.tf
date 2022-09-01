40a47a5887f63835f3c4473e183105a02d92888e
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-09-01 04:20:01 +0300

    Argocd (#33)
    * create helm release for argocd
    
    * create secret values for jenikins
    
    * create external secrets for jenkins credentials
    
    * use namespace variable

---
+++ helm_argocd.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "argocd" {
+  depends_on = [kubernetes_namespace.argocd]
+
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = var.argocd_namespace
+  version    = var.argocd_chart_version
+
+  set {
+    name = "dev.enabled"
+    value = false
+  }
+
+}
