57a0f1ac9fea915deec23de1a87ae3a96d71afb5
Philip Kwesiga <phil.kwesiga@gmail.com>
2022-09-01 10:45:36 +0000

    edit argo chart name

--- helm_argocd.tf
+++ helm_argocd.tf
@@ -7,7 +7,7 @@ resource "helm_release" "argocd" {
 
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
+  chart      = "argo"
   namespace  = var.argocd_namespace
   version    = var.argocd_chart_version
 
